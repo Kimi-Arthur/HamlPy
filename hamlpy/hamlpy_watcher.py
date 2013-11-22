52d5bc7b6f8a1a94f66853b6d2e534cccbeff6bc
Kimi@Arch530 <kimi.ribery@gmail.com>
2013-11-23 01:08:43 +0800

    2to3 conversion tool

--- hamlpy/hamlpy_watcher.py
+++ hamlpy/hamlpy_watcher.py
@@ -10,13 +10,8 @@ import codecs
 import os
 import os.path
 import time
-import hamlpy
-import nodes as hamlpynodes
-
-try:
-    str = unicode
-except NameError:
-    pass
+from . import hamlpy
+from . import nodes as hamlpynodes
 
 class Options(object):
     CHECK_INTERVAL = 3  # in seconds
@@ -70,7 +65,7 @@ def watch_folder():
     
     if args.verbose:
         Options.VERBOSE = True
-        print "Watching {} at refresh interval {} seconds".format(input_folder, args.refresh)
+        print("Watching {} at refresh interval {} seconds".format(input_folder, args.refresh))
     
     if args.extension:
         Options.OUTPUT_EXT = args.extension
@@ -136,18 +131,18 @@ def _compiled_path(destination, filename):
 def compile_file(fullpath, outfile_name, compiler_args):
     """Calls HamlPy compiler."""
     if Options.VERBOSE:
-        print '%s %s -> %s' % (strftime("%H:%M:%S"), fullpath, outfile_name)
+        print('%s %s -> %s' % (strftime("%H:%M:%S"), fullpath, outfile_name))
     try:
         if Options.DEBUG:
-            print "Compiling %s -> %s" % (fullpath, outfile_name)
+            print("Compiling %s -> %s" % (fullpath, outfile_name))
         haml_lines = codecs.open(fullpath, 'r', encoding = 'utf-8').read().splitlines()
         compiler = hamlpy.Compiler(compiler_args)
         output = compiler.process_lines(haml_lines)
         outfile = codecs.open(outfile_name, 'w', encoding = 'utf-8')
         outfile.write(output)
-    except Exception, e:
+    except Exception as e:
         # import traceback
-        print "Failed to compile %s -> %s\nReason:\n%s" % (fullpath, outfile_name, e)
+        print("Failed to compile %s -> %s\nReason:\n%s" % (fullpath, outfile_name, e))
         # print traceback.print_exc()
 
 if __name__ == '__main__':
